cdf06d46d4ea213529eaaf533574afe9a3a93948
progresor <regres@tuta.io>
2023-04-25 23:17:18 +0300

    app

---
+++ app/app.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import logging, os
+# logging.disable(logging.WARNING)         # disable all warning
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+# os.environ["FLASK_ENV"] = 'development'  # depricated
+# os.environ["FLASK_DEBUG"] = '1'
+
+import tensorflow as tf
+from tensorflow import keras
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+def set_params(param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12):
+
+    model = keras.models.load_model("saved_models/keras_model/")
+    prediction = model.predict([param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12])
+
+    return prediction[0][0]
+
+@app.route('/', methods=['POST', 'GET'])
+
+def app_calculation():
+    param_lst = []
+    message = ''
+    if request.method == 'POST':
+        
+        for i in range(1,13,1):
+            param = request.form.get(f'param{i}')
+            param_lst.append(float(param))
+            
+        message = set_params(*param_lst)
+
+    return render_template('template1.html', message=message) 
+
+# run app
+# python -m flask --app app.py run
+if __name__ == '__main__':
+    app.run()
